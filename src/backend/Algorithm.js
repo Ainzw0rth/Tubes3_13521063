38eb24ac30b948683af9c6074c23c2c9961f1bca
louis <louiscaesapdf@gmail.com>
2023-05-04 02:52:15 +0700

    algo almost done, TODO: insert data, update data, delete data

--- src/backend/Algorithm.js
+++ src/backend/Algorithm.js
@@ -1,9 +1,19 @@
+const { error } = require("console");
+
+/* -------- REGEX SECTION -------- */
 const pattern = {
     MATH : "mathExp",
     DATE : "dateExp",
     ADD : "addExp",
     DEL : "deleteExp",
     UNKNOWN : "unknownExp"
+}
+
+const regex = {
+    mathExprRegex : /^(\d+|\([^\(\)]*\))(?:\s*[+\-*/]+\s*(\d+|\([^\(\)]*\))|\s*[+\-*/]\s*[+\-*/]+?\s*(\d+|\([^\(\)]*\)))*$/,
+    dateRegex : /^(0?[1-9]|[12][0-9]|3[01])\|(0?[1-9]|1[012])\|\d{4}$/,
+    addRegex : /(.*tambah\s*pertanyaan)\s*(.*?)\s*dengan\s*jawaban\s*(.*)/i,
+    delRegex : /(.*hapus\s*pertanyaan)\s*(.*)/i
 }
 
 function standarizeQuestions(input) { // standarisasi input
@@ -15,14 +25,18 @@ function standarizeQuestions(input) { // standarisasi input
     input = input.trim();
 
     // ganti tanda-tanda spesial
-    input = input.replace(/[^+*/\-'^"?\w\s]|_/g, ""); // tanda-tanda yang diperbolehkan: +, -, /, *, ^, ', ", ?
+    input = input.replace(/[^+*/\-'^()"?|\w\s]|_/g, ""); // tanda-tanda yang diperbolehkan: +, -, /, *, ^, ', ", ?, |
 
     /* MASUKKAN DAFTAR PERTANYAAN */
     let listOfQuestions = [];
-    listOfQuestions.push([input, pattern.UNKNOWN, null]);
 
     /* PISAHKAN PERTANYAAN */
     const arrayOfQuestions = input.split("?").map((element) => element.trim()); // daftar pertanyaan
+
+    // hapus pertanyaan kosong terakhir, karena pertanyaan dipisahkan dengan ?, maka pertanyaan seperti siapa presiden indonesia yang pertama? "akan di split menjadi siapa presiden indonesia yang pertama" dan ""
+    if (arrayOfQuestions[arrayOfQuestions.length-1] == "" && arrayOfQuestions.length != 1) {
+        arrayOfQuestions.pop();
+    }
 
     for (let i = 0; i < arrayOfQuestions.length; i++) {
         listOfQuestions.push(findPattern(arrayOfQuestions[i])); // cari pattern dari setiap pertanyaan
@@ -32,51 +46,42 @@ function standarizeQuestions(input) { // standarisasi input
 }
 
 function findPattern(question) { // mencari pattern yang cocok untuk pertanyaan
-    const mathExprRegex = /^(\d+|\([^\(\)]*\))(?:\s*[\+\-\*\/]\s*(\d+|\([^\(\)]*\)))*$/;
-    const dateRegex = /^(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])\/\d{4}$/;
-    const addRegex = /(.*tambah\s*pertanyaan)\s*(.*?)\s*dengan\s*jawaban\s*(.*)/i;
-    const delRegex = /(.*hapus\s*pertanyaan)\s*(.*)/i;
-
     if (isItUnknown(question)) {
         return [question, pattern.UNKNOWN, null];
-    } else if (isItMath(question)) {
-        return [question.replace(mathExprRegex, "{math}"), pattern.MATH, question.match(mathExprRegex)[0]];
     } else if (isItDate(question)) {
-        return [question.replace(dateRegex, "{date}"), pattern.DATE, question.match(dateRegex)[0]];
+        return [question.replace(regex.dateRegex, "{date}"), pattern.DATE, question.match(regex.dateRegex)[0]];
+    } else if (isItMath(question)) {
+        return [question.replace(regex.mathExprRegex, "{math}"), pattern.MATH, question.match(regex.mathExprRegex)[0]];
     } else if (isItAdd(question)) {
-        return [question.replace(addRegex, "{add}"), pattern.ADD, question.match(addRegex)[0]];
+        return [question.replace(regex.addRegex, "{add}"), pattern.ADD, question.match(regex.addRegex)[0]];
     } else {
-        return [question.replace(delRegex, "{del}"), pattern.DEL, question.match(delRegex)[0]];
+        return [question.replace(regex.delRegex, "{del}"), pattern.DEL, question.match(regex.delRegex)[0]];
     }
 }
 
 function isItMath(question) {
-    const mathExprRegex = /^(\d+|\([^\(\)]*\))(?:\s*[\+\-\*\/]\s*(\d+|\([^\(\)]*\)))*$/;
-    return mathExprRegex.test(question);
+    return regex.mathExprRegex.test(question);
 }
 
 function isItDate(question) {
-    const dateRegex = /^(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])\/\d{4}$/;
-    return dateRegex.test(question);
+    return regex.dateRegex.test(question);
 }
 
 function isItAdd(question) {
-    const addRegex = /^(tambah)\s.+?\s(pertanyaan)$/;
-    return addRegex.test(question);
+    return regex.addRegex.test(question);
 }
 
 function isItDelete(question) {
-    const delRegex = /hapus pertanyaan/;
-    return delRegex.test(question);
+    return regex.delRegex.test(question);
 }
 
 function isItUnknown(question) {
     let ctr = 0;
-    if (isItMath(question)) {
+    if (isItDate(question)) {
         ctr++;
     }
 
-    if (isItDate(question)) {
+    if (isItMath(question)) {
         ctr++;
     }
 
@@ -90,37 +95,47 @@ function isItUnknown(question) {
 
     return ctr != 1;
 }
+/* -------- END OF REGEX SECTION -------- */
 
+
+/* -------- KMP SECTION -------- */
 function kmp(input, data) {
     let inputLength = input.length;
-    let dataLength = data.length;
+    
+    let k = 0;
+    let found = false;
+    let foundidx = null;
+    while (k < data.length && !found) {
+        let dataLength = data[k][0].length;
 
-    // cek apakah panjangnya sama
-    if (inputLength != dataLength) {
-        return [false, null];
-    }
-
-    let b = computeBorder(input);
-
-    let i = 0;
-    let j = 0;
-
-    while (i < dataLength) {
-        if (input[j] == data[i][0]) {
-            if (j == inputLength - 1) {
-                return [true, i];
-            }
-
-            i++;
-            j++;
-        } else if (j > 0) {
-            j = b[j-1];
+        // cek apakah panjangnya sama
+        if (inputLength != dataLength) {
+            k++;
         } else {
-            i++;
+            let b = computeBorder(input);
+    
+            let i = 0;
+            let j = 0;
+    
+            while (i < dataLength) {
+                if (input[j] == data[k][0][i]) {
+                    if (j == inputLength - 1) {
+                        found = true;
+                        foundidx = k;
+                    }
+    
+                    i++;
+                    j++;
+                } else if (j > 0) {
+                    j = b[j-1];
+                } else {
+                    i++;
+                }
+            }
+            k++;
         }
     }
-
-    return [false, null];
+    return [found, foundidx];
 }
 
 function computeBorder(pattern) {
@@ -146,43 +161,62 @@ function computeBorder(pattern) {
 
     return b;
 }
+/* -------- END OF KMP SECTION -------- */
 
+
+/* -------- BM SECTION -------- */
 function bm(input, data) {
     let last = buildLast(input);
-    let dataLength = data.length;
     let inputLength = input.length;
 
-    // cek apakah tidak mungkin exact match
-    if (dataLength != inputLength) {
-        return false;
-    }
-
-    let i = inputLength - 1;
-
-    if (i > dataLength - 1) {
-        return [false, null];
-    }
-
-    let j = inputLength - 1;
-    do {
-        if (input[j] == data[i][0]) {
-            if (j == 0) {
-                return [true, i];
-            } else {
-                i--;
-                j--;
-            }
+    let k = 0;
+    let found = false;
+    let foundidx = null;
+    while (k < data.length && !found) {
+        let dataLength = data[k][0].length;
+        let i = inputLength-1;
+        if (i > dataLength-1) {
+            k++;
         } else {
-            let lo = last[data[i][0]];
-            i = i + m - Math.min(j, lo+1);
-            j = m - 1;
+            let j = inputLength-1;
+            do {
+                if (input[j] == data[k][0][i]){
+                    if (j == 0) {
+                        found = true;
+                        foundidx = k;
+                    }
+                    else {
+                        i--;
+                        j--;
+                    }
+                } else {
+                    let lo = last[data[k][0][i]];
+                    i = i + inputLength - Math.min(j, 1+lo);
+                    j = inputLength - 1;
+                }
+            } while (i <= dataLength-1 && !found);
+            k++;
         }
-    } while (i <= n - 1);
-
-
-    return [false, null];
+    }
+    return [found, foundidx];
 }
 
+function buildLast(pattern) {
+    let last = [];
+    for (let i = 0; i < 128; i++) {
+        last[i] = -1;
+    }
+
+    for (let i = 0; i < pattern.length; i++) {
+        last[pattern[i]] = i;
+    }
+
+    return last;
+}
+/* -------- END OF BM SECTION -------- */
+
+
+/* -------- LEVENSHTEIN SECTION -------- */
 function levenshtein(input, data) {
     const m = input.length;
     const n = data.length;
@@ -209,35 +243,51 @@ function levenshtein(input, data) {
 
     return dp[m][n];
 }
+/* -------- END OF LEVENSHTEIN SECTION -------- */
 
-// fungsi untuk melakukan pembulatan perkalian seperti (0,33333...) * 3 agar menjadi 1
-function properlyRoundCalculation(number) {
-    if (Math.abs(number - 1) < 0.001) {
-        return Math.round(number);
+
+/* -------- CALCULATOR & DATE SECTION -------- */
+const calculationError = {
+    mathError : "Sintaks tidak valid",
+    dateError : "Tanggal tidak valid"
+}
+
+// fungsi untuk melakukan perhitungan matematika
+function calculate(number) {
+    try {
+        const result = eval(number);
+        return result;
+    } catch {
+        return calculationError.mathError;
     }
-
-    return number;
 }
 
 // fungsi untuk melakukan perhitungan hari dari input tanggal
 function getDayFromDate(datestring) {
-    // input berupa DD-MM-YYYY
+    // input berupa DD/MM/YYYY
+    const [day, month, year] = datestring.split("|");
     const daysList = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-    const dateComponents = datestring.split('-');
-    const standardizedDate = new Date(`${dateComponents[2]}-${dateComponents[1]}-${dateComponents[0]}`);
-    const result = daysList[standardizedDate.getDay()];
+    const standardizedDate = new Date(year, month-1, day);
 
-    return result;
+    if (standardizedDate.getDay() !== undefined) {
+        return daysList[standardizedDate.getDay()];
+    } else {
+        return calculationError.dateError;
+    }
 }
+/* -------- END OF CALCULATOR & DATE SECTION -------- */
 
+
+/* -------- FINDING PROPER RESPONSES SECTION -------- */
+// fungsi untuk mencari pertanyaan yang exact match menggunakan kmp/bm
 function findResponses(input, KMP, data) {
     // nanti masukin proses ngambil daftar pertanyaan dan response dari query terus masukin ke data
     /* INSERT HERE */
 
     const listOfQuestions = standarizeQuestions(input);
+    console.log("pertanyaan", listOfQuestions);
 
-    let listOfResponses = [data.length];
-
+    let listOfResponses = [];
 
     for (let i = 0; i < listOfQuestions.length; i++) {
         let [exact, index] = [null, null];
@@ -248,7 +298,7 @@ function findResponses(input, KMP, data) {
         }
 
         if (exact) {
-            listOfResponses[i] = data[i][1]; // ambil response dari hasil query dan masukkan ke daftar respons
+            listOfResponses[i] = generateResponse(listOfQuestions[i], data, index); // ambil response dari hasil query dan masukkan ke daftar respons            
         } else {
             listOfResponses[i] = null;
         }
@@ -257,14 +307,15 @@ function findResponses(input, KMP, data) {
     // berikan solusi
     for (let i = 0; i < listOfResponses.length; i++) {
         if (listOfResponses[i] == null) {
-            listOfResponses[i] = generateSolution(listOfQuestions[i], data);
+            listOfResponses[i] = findClosestSolution(listOfQuestions[i], data);
         }
     }
 
-    console.log("solusi", listOfResponses);
+    console.log(listOfResponses);
 }
 
-function generateSolution(question, data) {
+// fungsi untuk mencari pertanyaan ketika tidak ada yang exact match
+function findClosestSolution(question, data) {
     let rank = [];
 
     for (let i = 0; i < data.length; i++) {
@@ -274,16 +325,63 @@ function generateSolution(question, data) {
     rank.sort((a, b) => (b[1] - a[1]));
 
     // cek apakah ada yang mirip > 90%
-    console.log(rank);
     if (rank[0][1] >= 0.9) {
-        return "solusi"; // return solusi, liat querynya ntar harusnya data[i][0]
+        return generateResponse(question, data, rank[0][0]); // generate response untuk pertanyaan yang >= 90% mirip
     } else {
-        return "solusi123";
+        // generate 3 pertanyaan termirip
+        let solutions = "Pertanyaan tidak ditemukan di database.\nBerikut pertanyaan yang mirip:\n";
+        for (let i = 0; i < rank.length && i < 3; i++) {
+            solutions = solutions.concat(i+1);
+            solutions = solutions.concat(". ");
+            solutions = solutions.concat(data[rank[i][0]][0]);
+            solutions = solutions.concat("\n");
+        }
+        return solutions;
     }
 }
 
-let data = [["ques1", "solusi1"], ["ques2", "solusi2"], ["ques3", "solusi3"], ["ques4", "solusi4"]];
-findResponses("ques", true, data);
+// fungsi untuk melakukan kalkulasi untuk pertanyaan seperti kalkulator, tanggal, dll
+function generateResponse(question, data, idx) {
+    let solutions = "";
+    if (question[1] == pattern.MATH) {
+        if (calculate(question[2]) == calculationError.mathError) {
+            solutions = solutions.concat(calculationError.mathError);
+        } else {
+            solutions = solutions.concat(data[idx][1]);
+            solutions = solutions.concat(calculate(question[2]));
+        }
+    } else if (question[1] == pattern.DATE) {
+        if (getDayFromDate(question[2]) == calculationError.dateError) {
+            solutions = solutions.concat(calculationError.dateError);
+        } else {
+            solutions = solutions.concat(data[idx][1]);
+            solutions = solutions.concat(getDayFromDate(question[2]));
+        }
+    } else if (question[1] == pattern.ADD) {
+        // // TODO: query masuk database
+        // if (exists) {
+        //     // update
+        // } else {
+        //     // insert
+        // }
+        solutions = 1; // sementara
+    } else if (question[1] == pattern.DEL) {
+        // // TODO: query delete dari database
+        // if (exists) {
+        //     // delete
+        // } else {
+        //     solutions = solutions.concat("Tidak ada pertanyaan tersebut di database");
+        // }
+        solutions = 2; // sementara
+    } else {
+        solutions = solutions.concat(data[idx][1]);
+    }
+    return solutions;
+}
+/* -------- END OF FINDING PROPER RESPONSES SECTION -------- */
+
+
+
 
 module.exports = {
     standarizeQuestions,
@@ -297,8 +395,13 @@ module.exports = {
     computeBorder,
     bm,
     levenshtein,
-    properlyRoundCalculation,
+    calculate,
     getDayFromDate,
     findResponses,
-    generateSolution
+    findClosestSolution,
+    generateResponse
 }
+
+// /* TESTING PURPOSES */
+// let data = [["ques1", "solusi1"], ["ques2", "solusi2"], ["ques3", "solusi3"], ["ques4", "solusi4"], ["{date}", "hari "], ["{math}", "hasil "]];
+// findResponses("(1 - -1)*2?", true, data);
