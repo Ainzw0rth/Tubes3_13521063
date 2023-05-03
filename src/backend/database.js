e028867de2593d77a9d6b290a31b8d1db3aa09af
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-05-03 22:30:19 +0700

    testing

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,33 +1,21 @@
 const mongoose = require('mongoose');
 const Knowledge = require('../models/knowledge');
+const knowQuery = require('../query/knowQuery');
 
 mongoose.connect('mongodb+srv://13521063:ngechatgpt@chatbot.ynjyvpn.mongodb.net/chatbotweb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const newKnowledge1 = new Knowledge({
-  question: "Apa ibukota Indonesia?",
-  answer: "Ibukota Indonesia adalah Jakarta"
+const knowledge1 = new Knowledge({
+    question: 'Hapus pertanyaan {question}',
+    answer: 'Pertanyaan {question} telah dihapus'
 });
 
-const newKnowledge2 = new Knowledge({
-    question: "Apa mata kuliah IF semester 4 yang paling seru?",
-    answer: "Yang paling seru adalah STIMA tentunya :D"
-  });
+const knowledge2 = new Knowledge({
+    question: 'Hapus pertanyaan {question}',
+    answer: 'Tidak ada pertanyaan {question}'
+});
 
-newKnowledge1.save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-newKnowledge2.save()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+knowQuery.addKnowledge(knowledge1);
+knowQuery.addKnowledge(knowledge2);
--- src/backend/testing.js
+++ src/backend/testing.js
@@ -1,35 +1,16 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const axios = require('axios');
 const Chat = require('/Users/Asus/OneDrive - Institut Teknologi Bandung/Documents/GitHub/Tubes3_13521063/src/models/chats');
 const Knowledge = require('/Users/Asus/OneDrive - Institut Teknologi Bandung/Documents/GitHub/Tubes3_13521063/src/models/knowledge');
 const chatsRoutes = require('../routes/chatsRoutes');
-const chatsQuery = require('../query/chatsQuery');
 const knowRoutes = require('../routes/knowRoutes');
 const knowQuery = require('../query/knowQuery');
+const chatsQuery = require('../query/chatsQuery');
+const algo = require('/Users/Asus/OneDrive - Institut Teknologi Bandung/Documents/GitHub/Tubes3_13521063/src/backend/Algorithm');
 
-// express app
-const app = express();
-
-// middleware to handle json request body
-app.use(express.json());
-
-// add the Routes to the app
-app.use('/chats', chatsRoutes);
-app.use('/knowledges', knowRoutes);
-
-// connect to database (MongoDB)
 const dbURI = 'mongodb+srv://13521063:ngechatgpt@chatbot.ynjyvpn.mongodb.net/chatbotweb';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(4000))
     .catch((err) => console.log(err));
 
-// const newChat = new Chat({
-//     message: "8 + 5 * 2",
-//     is_bot_message: false
-// });
-
-// chatsQuery.addChat(newChat);
-
-const chats = knowQuery.getAllKnowledge();
-console.log(chats);
+const chat = chatsQuery.getChats();
+console.log(chat);
