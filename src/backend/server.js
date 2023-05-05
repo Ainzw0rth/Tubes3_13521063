3d9043b4da8ae70c3bb5410d3feb5ec690df3431
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-05-05 20:15:40 +0700

    rapiin server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const axios = require('axios');
-// const Chat = require('/Users/Asus/OneDrive - Institut Teknologi Bandung/Documents/GitHub/Tubes3_13521063/src/models/chats');
-// const Knowledge = require('/Users/Asus/OneDrive - Institut Teknologi Bandung/Documents/GitHub/Tubes3_13521063/src/models/knowledge');
 const chatsRoutes = require('../routes/chatsRoutes');
 const chatsQuery = require('../query/chatsQuery');
 const knowRoutes = require('../routes/knowRoutes');
@@ -28,13 +26,3 @@ const dbURI = 'mongodb+srv://13521063:ngechatgpt@chatbot.ynjyvpn.mongodb.net/cha
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(4000))
     .catch((err) => console.log(err));
-
-// const newChat = new Chat({
-//     message: "8 + 5 * 2",
-//     is_bot_message: false
-// });
-
-// chatsQuery.addChat(newChat);
-
-const chats = knowQuery.getAllKnowledge();
-console.log(chats);
