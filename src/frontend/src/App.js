a8ab15660e0e5387b7eb9eb88e20a0ebdc549ccc
irsyadnb <irsyadxd@gmail.com>
2023-05-05 21:32:38 +0700

    enchance ui

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
-import { Sidebar } from "./components/Sidebar";
-import { Mainbox } from "./components/Mainbox";
+import axios from "axios";
+import gpt from "./resources/ChatGPT.png";
+import saul from "./resources/saul.jpg";
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [currentMessage, setCurrentMessage] = useState("");
   const [previousChat, setPreviousChat] = useState([]);
   const [currentChatTitle, setCurrentChatTitle] = useState(null);
-  const [response, setResponse] = useState("");
+  const [algoButton, setAlgoButton] = useState(null);
 
   const emptyForm = useRef(null);
 
@@ -39,24 +40,125 @@ function App() {
     }
   }, [messages, currentChatTitle]);
 
+  // SideBar
+  const newChat = () => {
+    setCurrentMessage("");
+    setMessages([]);
+    setCurrentChatTitle(null);
+  };
+
+  const uniqueTitles = Array.from(
+    new Set(previousChat.map((prevChat) => prevChat.title))
+  );
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentChatTitle(uniqueTitle);
+    setMessages(null);
+    setCurrentMessage("");
+  };
+
+  // MainBox
+  function handleInputChange(event) {
+    setCurrentMessage(event.target.value);
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    // if (currentMessage !== "") {
+    //   setMessages([...messages, currentMessage]);
+    // }
+    console.log(currentMessage);
+    console.log("BUTTON:", algoButton);
+    axios
+      .post("http://localhost:4000/chats/answer", {
+        message: currentMessage,
+        useKMP: algoButton,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setMessages(response.data);
+        // Lakukan sesuatu dengan data response
+      })
+      .catch((error) => {
+        console.error(error);
+        // Lakukan sesuatu dengan error
+      });
+  }
+
+  const currentChat = previousChat.filter(
+    (previousChat) => previousChat.title === currentChatTitle
+  );
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // Scroll to bottom when messages is added
+    const element = ref.current;
+    setTimeout(() => {
+      element.scrollTop = element.scrollHeight;
+    }, 0);
+    // element.scrollTop = element.scrollHeight;
+  }, [messages]);
+
+  function kmpClick() {
+    setAlgoButton(true);
+  }
+
+  function bmClick() {
+    setAlgoButton(false);
+  }
+
   return (
     <div className="App">
-      <Sidebar
-        setCurrentMessage={setCurrentMessage}
-        setMessages={setMessages}
-        setCurrentChatTitle={setCurrentChatTitle}
-        previousChat={previousChat}
-      />
-      <Mainbox
-        messages={messages}
-        setMessages={setMessages}
-        currentMessage={currentMessage}
-        setCurrentMessage={setCurrentMessage}
-        previousChat={previousChat}
-        currentChatTitle={currentChatTitle}
-        emptyForm={emptyForm}
-        setResponse={setResponse}
-      />
+      <section className="sidebar">
+        <button className="newchat" onClick={newChat}>
+          + New Chat
+        </button>
+        <ul className="historylist">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              <div>{uniqueTitle}</div>
+            </li>
+          ))}
+        </ul>
+        <div className="algorithm-button">
+          <button className="kmpbutton" onClick={kmpClick}>
+            KMP
+          </button>
+          <button className="bmbutton" onClick={bmClick}>
+            BM
+          </button>
+        </div>
+      </section>
+      <section className="main">
+        <ul className="chat" ref={ref}>
+          {currentChat.map((message, index) => (
+            <li key={index} className={message.role}>
+              <div className="img-container">
+                <img
+                  src={message.role === "user" ? saul : gpt}
+                  className="imagelogo"
+                />
+              </div>
+              <div className="chat-container">
+                <p>{message.value}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="input">
+          <div className="input-container">
+            <form className="prompt" onSubmit={handleFormSubmit}>
+              <input
+                ref={emptyForm}
+                type="text"
+                onChange={handleInputChange}
+                placeholder="Type your message"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
